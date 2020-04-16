e9b5e6961210024a2e177ef2f9d7eff88349437b
shobhitsharma123 <63493884+shobhitsharma123@users.noreply.github.com>
2020-04-16 17:40:52 +0530

    3ed commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Going_Serverless_with_.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
